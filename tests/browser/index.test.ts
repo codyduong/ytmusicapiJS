e7e5f1d65e89e7cf81246185c00c9ae4213dacd5
Cody Duong <duongcody507@gmail.com>
2022-06-28 17:47:17 -0500

    add workflow dispatch

--- tests/browser/index.test.ts
+++
@@ -1,7 +0,0 @@
-import YTMusic from '../../src';
-
-test('Basic Construct', async () => {
-  const ytm = new YTMusic();
-  expect(ytm).toBeDefined();
-  expect(await ytm.search('Red Calliope')).toBeDefined();
-});
